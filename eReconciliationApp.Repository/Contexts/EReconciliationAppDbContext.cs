b4f8b3f67d053465d452a1d49cd83f4ddb44f19f
Arif ORAK <ariforak91@gmail.com>
2023-03-29 00:50:01 +0300

    Database bağlantısı hazırlandı, Configuration classları ve seed dataların eklenmesine müteakip migration oluşturulacak.

--- eReconciliationApp.Repository/Contexts/EReconciliationAppDbContext.cs
+++ eReconciliationApp.Repository/Contexts/EReconciliationAppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,7 @@ namespace eReconciliationApp.Repository.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(modelBuilder);
         }
         public DbSet<AccountReconciliation> AccountReconciliations { get; set; } 
